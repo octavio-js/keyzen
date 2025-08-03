02aa00e4e7d40d55fbb404c9fb20294ef983e8b6
octavio <vizdoagaoctavio33@gmail.com>
2025-08-03 21:41:04 +0300

    Create and style ThemeSelection

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header/Header.jsx';
 import TextArea from './components/TextArea/TextArea.jsx';
+import ThemeSelection from './components/ThemeSelection/ThemeSelection.jsx';
 
 export default function App() {
   return (
     <>
       <Header/>
       <TextArea/>
+      <ThemeSelection/>
     </>
   );
 }
