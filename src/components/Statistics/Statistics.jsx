5365f29475df6b983a2747ddedf08f73a8a99b1b
octavio <vizdoagaoctavio33@gmail.com>
2025-08-04 18:42:02 +0300

    Make statistics functionality work

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './statistics.css';
 
-export default function Statistics() {
+export default function Statistics(props) {
+  const accuracy = props.letters.length > 0 ? Math.floor((props.correctChars*100) / props.letters.length) : 0;
+
   return (
-    <div>
-      <p id="accuracy">Accuracy: 0%</p>
-      <p id="stats" title="Total / Correct / Incorrect / Missed Spaces">0 / 0 / 0 / 0</p>
+    <div id="statistics">
+      <p style={{display: props.accuracyDisplay}} id="accuracy">Accuracy: {accuracy}%</p>
+      <p style={{display: props.statsDisplay}} id="stats" title="Total / Correct / Incorrect / Missed Spaces">
+        {props.letters.length} / {props.correctChars} / {props.incorrectChars} / {props.missedSpaces}
+      </p>
     </div>
   );
 }
